328de77a9e681129b8fb04e68622db5d83ead587
Pauline Pecorini <pauline.pecorini@gmail.com>
2016-08-22 15:34:54 +0200

    Festivals on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Festival.destroy_all
+
+Festival.create!(name: "Lollapaloozade", start_date: Date.new(2016,9,10) , end_date: Date.new(2016,9,11) , location: "Berlin", photo: "http://lolp3.de.c3cdn.com/wp/wp-content/uploads/2014/11/LOL2014-DE-footer_v2.png")
+Festival.create!(name: "Fête de l'Huma", start_date: Date.new(2016,9,9) , end_date: Date.new(2016,9,11) , location: "La Courneuve", photo: "http://adecr44.fr/sites/default/files/fichiers/www/images/Fete-de-lhuma-bandeau-960x450.png")
+Festival.create!(name: "Roscella Bay", start_date: Date.new(2016,9,23), end_date: Date.new(2016,9,25), location: "La Rochelle" , photo: "http://www.roscellabay.fr/img/logo_video_fb-2-1.gif")
+
+Concert.new
