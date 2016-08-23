7034418ef17ad161e1cb2eab8fb1b3fc1012cc78
Oscar Friedel <ofriedel@gmail.com>
2016-08-23 15:22:29 +0200

    concert by day done

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,4 +2,6 @@ class Concert < ApplicationRecord
   belongs_to :festival
   belongs_to :artist
   has_many :events
+
+  scope :of_the_day, -> (datetime) { where("? <= start_time AND start_time < ?", datetime, datetime + 1.day)}
 end
