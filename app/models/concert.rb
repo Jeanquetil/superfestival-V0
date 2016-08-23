b4bd0b2b162a658bc4d2ac28fac9fb01d0ddfa89
Damien Le Thiec <damien.lethiec@gmail.com>
2016-08-23 15:04:18 +0200

    test

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,5 +1,12 @@
 class Concert < ApplicationRecord
+  include AlgoliaSearch
+
   belongs_to :festival
   belongs_to :artist
   has_many :events
+
+  algoliasearch do
+    attributesToIndex ['stage']
+    customRanking ['artist_id']
+  end
 end
--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,6 +1,13 @@
 class Festival < ApplicationRecord
+  include AlgoliaSearch
+
   has_many :concerts
   has_many :timetables
 
   validates :name, presence: true
+
+  algoliasearch do
+    attributesToIndex ['name, location']
+    customRanking ['start_date']
+  end
 end
