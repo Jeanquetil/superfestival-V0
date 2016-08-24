75fc875d2ef05b60ea704f22f5b0bbe3b0304360
Damien Le Thiec <damien.lethiec@gmail.com>
2016-08-24 11:31:53 +0200

    commit du matin

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,8 @@
 class Artist < ApplicationRecord
-  include AlgoliaSearch
 
   has_many :artists
 
   validates :name, presence: true
   validates :genre, presence: true
 
-  algoliasearch do
-    attributesToIndex ['name', 'genre']
-    customRanking ['desc(name)']
-  end
 end
