f92603a84a660d441239d0b725cccba4499fa742
Oscar Friedel <ofriedel@gmail.com>
2016-08-30 12:16:59 +0200

    Conditonal formating stays on reload, but not on search

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function setImpossibleEvents(impossibleIds) {
+  $('.hit').each(function() {
+    $(this).parent().removeClass('impossible');
+    var id = parseInt($(this).attr('id'));
+    var that = $(this);
+    if(impossibleIds.includes(id)) {
+      that.parent().addClass('impossible');
+    }
+  });
+}
